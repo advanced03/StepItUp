6093a942dd62286139b4b94d99ebf748d79d9330
JordiPan <jordipan.jp@gmail.com>
2022-12-11 16:33:20 +0100

    je kan naar lege creer account pagina gaan

--- app/src/main/java/com/sof/stepitup/MainActivity.java
+++ app/src/main/java/com/sof/stepitup/MainActivity.java
@@ -2,6 +2,7 @@ package com.sof.stepitup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -22,27 +23,29 @@ public class MainActivity extends AppCompatActivity {
         TextView password =(TextView) findViewById(R.id.password);
 
         MaterialButton loginbtn = (MaterialButton) findViewById(R.id.loginbtn);
-        TextView createAccount = (TextView) findViewById(R.id.noacc);
 
+        TextView createAccount = (TextView) findViewById(R.id.noacc);
+//        TextView loginThing = (TextView) findViewById(R.id.login);
         createAccount.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Toast.makeText(MainActivity.this,"Succesvol Ingelogd!",Toast.LENGTH_SHORT).show();
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CreateAcc.class);
+                startActivity(intent);
             }
         });
         //admin and admin
 
-        loginbtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(username.getText().toString().equals("beheerder") && password.getText().toString().equals("beheerder")){
-                    //correct
-                    Toast.makeText(MainActivity.this,"Succesvol Ingelogd!",Toast.LENGTH_SHORT).show();
-                }else
-                    //incorrect
-                    Toast.makeText(MainActivity.this,"Wachtwoord of gebruikersnaam fout!",Toast.LENGTH_SHORT).show();
-            }
-        });
+//        loginbtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                if(username.getText().toString().equals("beheerder") && password.getText().toString().equals("beheerder")){
+//                    //correct
+//                    Toast.makeText(MainActivity.this,"Succesvol Ingelogd!",Toast.LENGTH_SHORT).show();
+//                }else
+//                    //incorrect
+//                    Toast.makeText(MainActivity.this,"Wachtwoord of gebruikersnaam fout!",Toast.LENGTH_SHORT).show();
+//            }
+//        });
 
 
     }
