338d4768b5c9f4c3238e5d1ef133f2ffb9337d17
JordiPan <jordipan.jp@gmail.com>
2022-12-12 15:51:23 +0100

    start van connection gemaakt

--- app/src/main/java/com/sof/stepitup/CreateAcc.java
+++ app/src/main/java/com/sof/stepitup/CreateAcc.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.button.MaterialButton;
 import com.vishnusivadas.advanced_httpurlconnection.FetchData;
@@ -18,35 +21,49 @@ public class CreateAcc extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_acc);
 
-        MaterialButton loginbtn = (MaterialButton) findViewById(R.id.loginbtn);
-
-        //Start ProgressBar first (Set visibility VISIBLE)
-        Handler handler = new Handler(Looper.getMainLooper());
-        handler.post(new Runnable() {
+        MaterialButton createBtn = (MaterialButton) findViewById(R.id.createacc_btn);
+        TextView usernameId =(TextView) findViewById(R.id.user);
+        TextView passwordId =(TextView) findViewById(R.id.password);
+        TextView repeatId =(TextView) findViewById(R.id.repeatPassword);
+        String username = usernameId.getText().toString();
+        String password = passwordId.getText().toString();
+        String repeat = repeatId.getText().toString();
+        createBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void run() {
-                //Starting Write and Read data with URL
-                //Creating array for parameters
-                String[] field = new String[3];
-                field[0] = "username";
-                field[1] = "password";
-                field[2] = "repeatPassword";
-                //Creating array for data
-                String[] data = new String[2];
-                data[0] = "data-1";
-                data[1] = "data-2";
-                PutData putData = new PutData("https://projects.vishnusivadas.com/AdvancedHttpURLConnection/putDataTest.php", "POST", field, data);
-                if (putData.startPut()) {
-                    if (putData.onComplete()) {
-                        String result = putData.getResult();
-                        //End ProgressBar (Set visibility to GONE)
-                        Log.i("PutData", result);
-                    }
+            public void onClick(View view) {
+                if (!username.equals("") && !password.equals("") && !repeat.equals("")) {
+                    //Start ProgressBar first (Set visibility VISIBLE)
+                    Handler handler = new Handler(Looper.getMainLooper());
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            //Starting Write and Read data with URL
+                            //Creating array for parameters
+                            String[] field = new String[3];
+                            field[0] = "username";
+                            field[1] = "password";
+                            field[2] = "repeatPassword";
+                            //Creating array for data
+                            String[] data = new String[3];
+                            data[0] = username;
+                            data[1] = password;
+                            data[2] = repeat;
+                            PutData putData = new PutData("https://projects.vishnusivadas.com/AdvancedHttpURLConnection/putDataTest.php", "POST", field, data);
+                            if (putData.startPut()) {
+                                if (putData.onComplete()) {
+                                    String result = putData.getResult();
+                                    //End ProgressBar (Set visibility to GONE)
+                                    Log.i("PutData", result);
+                                }
+                            }
+                            //End Write and Read data with URL
+                        }
+                    });
                 }
-                //End Write and Read data with URL
+                else {
+                    Toast.makeText(getApplicationContext(), "work bitsh", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
-
-
 }
