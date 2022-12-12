638d2cbd616089ad37d9882349450d76114ddccf
JordiPan <jordipan.jp@gmail.com>
2022-12-12 15:32:52 +0100

    bugs met xml files opgelost

--- app/src/main/java/com/sof/stepitup/CreateAcc.java
+++ app/src/main/java/com/sof/stepitup/CreateAcc.java
@@ -3,6 +3,13 @@ package com.sof.stepitup;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
+
+import com.google.android.material.button.MaterialButton;
+import com.vishnusivadas.advanced_httpurlconnection.FetchData;
+import com.vishnusivadas.advanced_httpurlconnection.PutData;
 
 public class CreateAcc extends AppCompatActivity {
 
@@ -10,5 +17,36 @@ public class CreateAcc extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_acc);
+
+        MaterialButton loginbtn = (MaterialButton) findViewById(R.id.loginbtn);
+
+        //Start ProgressBar first (Set visibility VISIBLE)
+        Handler handler = new Handler(Looper.getMainLooper());
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                //Starting Write and Read data with URL
+                //Creating array for parameters
+                String[] field = new String[3];
+                field[0] = "username";
+                field[1] = "password";
+                field[2] = "repeatPassword";
+                //Creating array for data
+                String[] data = new String[2];
+                data[0] = "data-1";
+                data[1] = "data-2";
+                PutData putData = new PutData("https://projects.vishnusivadas.com/AdvancedHttpURLConnection/putDataTest.php", "POST", field, data);
+                if (putData.startPut()) {
+                    if (putData.onComplete()) {
+                        String result = putData.getResult();
+                        //End ProgressBar (Set visibility to GONE)
+                        Log.i("PutData", result);
+                    }
+                }
+                //End Write and Read data with URL
+            }
+        });
     }
+
+
 }
