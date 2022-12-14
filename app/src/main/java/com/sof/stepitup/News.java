0c59132ef10dcaedd6a13dff7f2fc00d07000de5
ybn15 <22093117@student.hhs.nl>
2022-12-14 15:56:03 +0100

    news pagina gemaakt

---
+++ app/src/main/java/com/sof/stepitup/News.java
@@ -0,0 +1,13 @@
+package com.sof.stepitup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class News extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.news_activity);
+    }
+}
