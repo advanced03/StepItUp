6014667270b57a3d080ebc5e9895f6fbeace7a1a
AdvancedGaming <incedalsrf@gmail.com>
2023-01-11 11:48:32 +0100

    Aantal wijzigingen en navigatiebalk aan bijna alle activities toegevoegd

--- app/src/main/java/com/sof/stepitup/NewsFragment.java
+++ app/src/main/java/com/sof/stepitup/NewsFragment.java
@@ -16,5 +16,6 @@ public class NewsFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_news, container, false);
+
     }
 }
