6dabdc4291412548db6c4c29b3618c566cab22ab
JordiPan <jordipan.jp@gmail.com>
2023-01-15 18:20:28 +0100

    home pagina verbetert

--- app/src/main/java/com/sof/stepitup/HomeFragment.java
+++ app/src/main/java/com/sof/stepitup/HomeFragment.java
@@ -10,6 +10,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.sof.stepitup.session.SessionManager;
@@ -17,6 +18,10 @@ import com.vishnusivadas.advanced_httpurlconnection.PutData;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 
 public class HomeFragment extends Fragment {
@@ -28,7 +33,11 @@ public class HomeFragment extends Fragment {
         // Inflate the layout for this fragment
         SessionManager sessionManager = new SessionManager(root.getContext(),"userSession");
 //        TextView welcome = root.findViewById(R.id.welcome_txt);
-//        TextView steps = root.findViewById(R.id.steps);
+        TextView steps = root.findViewById(R.id.user_steps);
+        TextView date = root.findViewById(R.id.text_today);
+        TextView distance = root.findViewById(R.id.text_walking_distance);
+        TextView calories = root.findViewById(R.id.calories);
+        ProgressBar progressBar = root.findViewById(R.id.pBar);
 //        zet username in welkom
 //        String welcomeText = getString(R.string.welkom, sessionManager.getUserInfo()[1]);
 //        welcome.setText(welcomeText);
@@ -43,26 +52,41 @@ public class HomeFragment extends Fragment {
             }
         });
         //query voor user data
-//        String[] field = new String[1];
-//        field[0] = "user_ID";
-//        String[] data = new String[1];
-//        data[0] = sessionManager.getUserInfo()[0].toString();
-//
-//        PutData putData = new PutData("http://" + ip + "/stepitup/GetLatestUserInfo.php", "GET", field, data);
-//        if (putData.startPut()) {
-//            if (putData.onComplete()) {
-//                try {
-//                    String result = putData.getResult();
-//                    JSONObject user = new JSONObject(result);
-//                    int userSteps = user.getInt("stappen");
-//                    sessionManager.updatePoints(user.getInt("punten"));
-//                    String a = getString(R.string.steps, userSteps);
-//                    steps.setText(a);
-//                } catch (JSONException e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//        }
+        String[] field = new String[1];
+        field[0] = "user_ID";
+        String[] data = new String[1];
+        data[0] = sessionManager.getUserInfo()[0].toString();
+
+        PutData putData = new PutData("http://" + ip + "/stepitup/GetLatestUserInfo.php", "GET", field, data);
+        if (putData.startPut()) {
+            if (putData.onComplete()) {
+                try {
+                    String result = putData.getResult();
+                    JSONObject user = new JSONObject(result);
+                    int userSteps = user.getInt("stappen");
+                    sessionManager.updatePoints(user.getInt("punten"));
+                    String a = getString(R.string.steps, userSteps);
+                    steps.setText(a);
+
+                    String totalCal = String.format("%.1f", userSteps*0.4);
+                    String cal = getString(R.string.cal, totalCal); //elke stap is .4 cal
+                    calories.setText(cal);
+
+                    String totalKm = String.format("%.3f", userSteps/1200.0);
+                    String km = getString(R.string.kilometers,totalKm);//1200 ongeveer voor 1 km
+                    distance.setText(km);
+
+                    SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yy");
+                    String dateFormatted = sdf.format(new Date());
+                    String today = getString(R.string.date, dateFormatted);
+                    date.setText(today);
+
+                    progressBar.setProgress(userSteps);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
 
         SwipeRefreshLayout swipeRefreshLayout = root.findViewById(R.id.swiperefreshlayout);
         swipeRefreshLayout.setOnRefreshListener(
